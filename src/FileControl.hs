7e06d4031ceb05007fecbb5e7caea6be404ffffa
Kozlov <wizardng1@gmail.com>
2021-05-03 22:15:29 +0300

    Module of check file mode added

---
+++ src/FileControl.hs
@@ -0,0 +1,54 @@
+module FileControl (
+                     isOwner
+                   , isGroup
+                   , isExec
+                   , isRead
+                   , isWrite
+                   ) where
+    
+import Control.Monad
+
+import System.Posix.Files
+import System.Posix.Types
+import System.FilePath.Posix
+import System.Posix.User
+
+isOwner :: FilePath -> IO Bool
+isOwner f = liftM2 (==) (fileOwner <$> getFileStatus f) getRealUserID  
+
+isGroup :: FilePath -> IO Bool
+isGroup f = liftM2 (==) (fileGroup <$> getFileStatus f) getRealGroupID 
+
+checkMode :: FileMode -> FileMode -> Bool
+checkMode fm cm = cm == intersectFileModes fm cm
+
+isExec :: FilePath  -> IO Bool
+isExec fp = do
+    fm <- fileMode <$> getFileStatus fp
+    own <- isOwner fp
+    grp <- isGroup fp
+    let ex =   (own &&checkMode fm ownerExecuteMode) 
+            || (grp &&checkMode fm groupExecuteMode)
+            || checkMode fm otherExecuteMode
+    return ex
+
+isRead :: FilePath  -> IO Bool
+isRead fp = do
+    fm <- fileMode <$> getFileStatus fp
+    own <- isOwner fp
+    grp <- isGroup fp
+    let ex =   (own &&checkMode fm ownerReadMode) 
+            || (grp &&checkMode fm groupReadMode)
+            || checkMode fm otherReadMode
+    return ex
+
+isWrite :: FilePath  -> IO Bool
+isWrite fp = do
+    fm <- fileMode <$> getFileStatus fp
+    own <- isOwner fp
+    grp <- isGroup fp
+    let ex =   (own &&checkMode fm ownerWriteMode) 
+            || (grp &&checkMode fm groupWriteMode)
+            || checkMode fm otherWriteMode
+    return ex
+
--- src/ReadMenu.hs
+++ src/ReadMenu.hs
@@ -11,6 +11,17 @@ module ReadMenu (
                 , readMenu
                 ) where
 
+import System.IO
+import System.Posix.Env
+import System.FilePath.Posix
+import System.Directory
+
+import Data.List 
+import Data.Functor
+import Util
+
+import FileControl
+
 data MenuItem = MenuItem { name :: String
                          , action :: String}
                          deriving (Show, Eq)
@@ -35,3 +46,22 @@ readUserComponent = [SubLevel
                             (Submenu "User" (Menu 
                                         [Item (MenuItem "Telegram" "Telegram")]
                       ))]
+
+readPath :: IO [FilePath]
+readPath = getEnv "PATH" <&> parsePath
+
+parsePath :: Maybe String -> [FilePath]
+parsePath Nothing = []
+parsePath (Just a) = splitSearchPath a
+
+readFiles :: FilePath -> [MenuPunkt]
+readFiles _ = []
+
+readFileList :: FilePath -> ([FilePath] -> [FilePath]) -> IO [FilePath]
+readFileList path fltr = do
+    l1 <- getDirectoryContents path
+    let l2 = fltr l1
+    let l3 = map (path </>) l2
+    return l3 
+
+
