076daaeb573fb1afc1317de0b125254e537d2c80
Kozlov <wizardng1@gmail.com>
2022-01-12 22:39:53 +0300

    Remove aeson

--- app/MenuTypes.hs
+++ app/MenuTypes.hs
@@ -3,32 +3,33 @@
 {-# LANGUAGE ConstrainedClassMethods #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiWayIf #-}
 
 module MenuTypes(
                   MenuItem (..)
                 , Submenu (..)
                 , MenuElement (..)
                 , Menu (..)
+                , makeMenu
                 )where 
 
 --import qualified    Data.ByteString as B
 --import              Control.Applicative((<*>), (<$>))
-import  Control.Monad (mzero)
+--import  Control.Monad (mzero)
 --import  GHC.List(length)
 import  GHC.Generics
-import  Data.Aeson
-import  Data.Aeson.KeyMap ( toList)
-import  Data.Aeson.Key( toString
-                      , fromString)
---import qualified    Data.Text.Internal as DT
+import qualified GHC.Base as B
+import qualified GHC.List as L
+import Data.Text
+import qualified Data.Bifunctor as DB 
 
 -- | Type of elementary menu item 
-data MenuItem = MenuItem { name :: String
-                         , action :: String}
+data MenuItem = MenuItem { name :: Text
+                         , action :: Text}
                          deriving (Generic, Show, Eq)
 
 -- | Type of menu sublevel
-data Submenu = Submenu { name :: String
+data Submenu = Submenu { name :: Text
                        , sub :: Menu}
                        deriving (Generic, Show, Eq)
 
@@ -38,48 +39,32 @@ data MenuElement = Item MenuItem | SubLevel Submenu deriving (Generic, Show, Eq)
 -- | Main type of menu
 newtype Menu = Menu [MenuElement] deriving (Generic, Show, Eq)
 
--- JSON instances from menu item
-instance FromJSON MenuItem where
-  parseJSON (Object m) = if length l /= 1 then mzero
-                                         else MenuItem n <$> m .: k 
-                                           where l = toList m
-                                                 k = fst $ head l
-                                                 n = toString k
-  parseJSON _ = mzero
-                                  
-instance ToJSON MenuItem where
-  toJSON (MenuItem n a) = object [fromString n .= a ]
+makeMenu :: Text -> Menu
+makeMenu t = Menu $ makeMEList $ Data.Text.lines t 
+
+makeMEList :: [Text] -> [MenuElement]
+makeMEList t = fst $ makeHelper t
+--makeMEList [] = []
+--makeMEList (x:xs) = makeME x : makeMEList xs
+
+makeHelper :: [Text] -> ([MenuElement], [Text])
+makeHelper [] = ([], [])
+makeHelper (τ:τσ) = if | t1=="]" -> ([], τσ)
+                       | t2 == "[" -> (DB.first (SubLevel (Submenu t1 (Menu τ')) :) (makeHelper τσ'))
+                       | (t1, t2) == ("","") -> (τ', τσ')
+                       | otherwise -> (DB.first (makeME τ :) (makeHelper τσ))
+    where (t1, t2) = parseStr τ
+          (τ', τσ') = makeHelper τσ
+
+parseStr :: Text -> (Text, Text)
+parseStr τ = if  | l==2 -> (L.head t, L.last t)
+                 | l==1 -> (L.head t, "")
+                 | otherwise -> ("","")
+                 where t = B.map strip $ splitOn ":" $ L.head $ splitOn "#" τ
+                       l = L.length t
 
 
--- JSON instances from submenu
-instance FromJSON Submenu where
-  parseJSON (Object m) = if length l /= 1 then mzero
-                                         else Submenu n <$> m .: k 
-                                           where l = toList m
-                                                 k = fst $ head l
-                                                 n = toString k
-  parseJSON _ = mzero
---
-instance ToJSON Submenu where 
-  toJSON (Submenu n s) = object [fromString n .= toJSON s]
-
-
--- JSON instances from  menu elements
-instance FromJSON MenuElement where
-  parseJSON _ = mzero
-
-instance ToJSON MenuElement where
-  toJSON (Item i) = toJSON i
-  toJSON (SubLevel s) = toJSON s
-
--- JSON instances from menu
-instance FromJSON Menu where
-  parseJSON (Object m) = if length l /= 1 then mzero
-                                         else Menu <$> m .: k 
-                                           where l = toList m
-                                                 k = fst $ head l
-  parseJSON _ = mzero
-
-instance ToJSON Menu where
-  toJSON (Menu m) =  toJSON $ fmap toJSON m
-
+makeME :: Text -> MenuElement
+--makeME x xs = Item $ MenuItem "1" "2"
+makeME σ = Item $ MenuItem φ λ
+                where (φ,λ) = parseStr σ 
--- app/ReadMenu.hs
+++ app/ReadMenu.hs
@@ -2,14 +2,15 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE ConstrainedClassMethods #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module ReadMenu ( readMenu
                 , readSystemComponent
                 , readPath
                 , readSystemItems
                 , readUM
-                , decoUM
-                , parseME
+                , parseM
+                --, hr, h1
                 ) where
 
 import System.Posix.Env
@@ -20,10 +21,13 @@ import System.Directory
 import Data.Functor
 import Control.Monad
 
-import Data.Aeson
-import Data.Aeson.KeyMap (toList)
+--import Data.Aeson
+--import Data.Aeson.KeyMap (toList)
+--import Data.Aeson.Types (parse)
 --import qualified Data.ByteString.Lazy.Internal.ByteString as B
-import qualified Data.ByteString.Lazy as B
+--import qualified Data.ByteString.Lazy as B
+import Data.Text
+import qualified Data.Text.IO as TIO
 
 import FileControl
 import MenuTypes
@@ -35,6 +39,7 @@ menuFile = "/home/hask/conf/wilaun/menu"
 
 
 readMenu :: IO Menu
+--readMenu =  Menu <$> ( (++) <$> readSystemComponent  <*> readUM) -- serComponent
 readMenu =  Menu <$> ( (++) <$> readSystemComponent  <*> readUM) -- serComponent
 
 readSystemComponent :: IO [MenuElement]
@@ -42,7 +47,7 @@ readSystemComponent = do
     p <- readPath
     pp <- filterM fileExist p
     l <- traverse readSystemItems pp 
-    pure [SubLevel (Submenu "System"  $ Menu l )]
+    pure [SubLevel (Submenu "System" $ Menu l )]
 
 readPath :: IO [FilePath]
 readPath = getEnv "PATH" <&> parsePath
@@ -53,14 +58,14 @@ parsePath (Just a) = splitSearchPath a
 
 readSystemItems :: FilePath -> IO MenuElement
 readSystemItems p = do 
-    ff <- fmap (map (p </>)) (listDirectory p) 
+    ff <- fmap (Prelude.map (p </>)) (listDirectory p) 
     f <- filterM fileExist ff
-    me <- filterMenu [Item $ MenuItem x (p </> x) | x<-f]
-    pure $ SubLevel $ Submenu p (Menu me)
+    me <- filterMenu [Item $ MenuItem (pack x) (pack (p </> x)) | x<-f]
+    pure $ SubLevel $ Submenu (pack p) (Menu me)
 
 filterMenu :: [MenuElement] -> IO [MenuElement]
 filterMenu (x@(Item (MenuItem _ ex)) : xs) = do
-            f <- isExec ex 
+            f <- isExec (unpack ex) 
             if f 
                then  fmap (x :) (filterMenu xs) 
                else filterMenu xs
@@ -69,24 +74,11 @@ filterMenu x = pure x
 readUM :: IO [MenuElement]
 readUM = do
     fm <- fileExist menuFile
-    if fm then do
-            f <- B.readFile menuFile
-            pure $ parseMenu (decode f :: Maybe Value)
-        else pure [] 
--- Tempore function. Delete after debuging
-decoUM :: IO [(Key, Value)]
-decoUM = do 
-            f <- B.readFile menuFile
-            let o = (decode f::Maybe Object)
-            let ll = maybe [] toList o
-            pure ll
+    l <- if fm then parseM
+               else pure $ Menu [] 
+    pure [SubLevel (Submenu "User " l )]
 
-parseMenu ::Maybe Value -> [MenuElement]
-parseMenu Nothing = []
-parseMenu (Just a) = parseME a
-
-parseME :: Value -> [MenuElement]
-parseME (Array a) = if null a then []
-                                else [Item $ MenuItem "1" "2"]
---parseME (Array a) = [parseMenu (Just x) | x <- a]                               
-parseME _ = []
+parseM ::  IO Menu
+parseM = do 
+            f <- TIO.readFile menuFile
+            pure $ makeMenu f
